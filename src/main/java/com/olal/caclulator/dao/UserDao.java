41743fca9886fd5b4d72c94e5a101a1b12af3701
Oleg Kostin <o.kostin13@gmail.com>
2018-01-05 00:16:59 +0300

    removing

--- src/main/java/com/olal/caclulator/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.olal.caclulator.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.io.Serializable;
-
-public interface UserDao extends  CrudRepository<Integer,Integer>, Serializable{
-}
